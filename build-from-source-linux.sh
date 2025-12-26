6715f96248f5a36e6ca0aab7d121abcab82e706c
michal92299 <chudybyczek2137@gmail.com>
2025-12-26 17:21:29 +0100

    Update build-from-source-linux.sh

--- build-from-source-linux.sh
+++ build-from-source-linux.sh
@@ -1,31 +1,13 @@
 #!/bin/bash
-# windows -> x86_64-w64-mingw32-g++ main.cpp -o program.exe
 cd source
-cd plsa
-g++ main.cpp -o plsa
-cd ..
-cd translator
-cargo build --release
-cd ..
 cd compiler
 cargo build --release
 cd ..
-cd interpreter 
-cargo build --release
-cd ..
-cd vm
-cargo build --release
+cd plsa
+g++ main.cpp -o plsa
 cd ..
 cd updater
 go get updater
-go build 
-cd ..
-cd ..
-cd cli
-cd vira
-go get vira
 go build
 cd ..
-cd virac
-go get virac
-go build
+cd 
