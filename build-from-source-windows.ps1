89e843ead573a0aec7dc59180005ea98bce3ba15
michal92299 <chudybyczek2137@gmail.com>
2025-12-26 20:51:22 +0100

    Update build-from-source-windows.ps1

--- build-from-source-windows.ps1
+++ build-from-source-windows.ps1
@@ -1,1 +1,48 @@
+# build.ps1
+
+# source/compiler
+Set-Location source/compiler
+cargo build --release
+
+# source/plsa
+Set-Location ..
+Set-Location plsa
+g++ main.cpp -o plsa
+
+# source/updater
+Set-Location ..
+Set-Location updater
+go get updater
+go build
+
+# source/diagnostic
+Set-Location ..
+Set-Location diagnostic
+cargo build --release
+
+# source/preprocessor
+Set-Location ..
+Set-Location preprocessor
+gcc main.c -o preprocessor
+
+# powrót do root
+Set-Location ..
+Set-Location ..
+
+# cli/vira
+Set-Location cli/vira
+go get vira
+go build
+
+# cli/virac
+Set-Location ..
+Set-Location virac
+go get virac
+go build
+
+# cli/virus
+Set-Location ..
+Set-Location virus
+go get virus
+go build
 
